8fa02b6e60d9fc8813caf3b8113de3335c73b5ee
nelson-caberto <nelson-caberto@users.noreply.github.com>
2020-05-02 00:03:04 -0500

    added episodes route

--- tvdb.py
+++ tvdb.py
@@ -14,6 +14,10 @@ class tvdb:
         200:"success"
     }
 
+    def __handleException(self, status_codes, status_code):
+        if (status_code != 200):
+            raise Exception(status_codes[status_code])
+
     def __init__(self, userkey, apikey, username):
         self.userkey = userkey
         self.apikey = apikey
@@ -28,22 +32,29 @@ class tvdb:
             'username':self.username
         }
         response = requests.post(url, data=json.dumps(data), headers=self.postHeader)
-        status_code = response.status_code
         status_codes = self.response_status_codes
         status_codes[401] = "Invalid credentials and/or API token"
-
-        if (status_code != 200):
-            raise Exception(status_codes[status_code])
-
+        self.__handleException(status_codes, response.status_code)
         self.token = response.json()['token']
         self.getHeader['Authorization'] = 'Bearer ' + self.token
 
     def refresh_token(self):
         url = self.url + '/refresh_token'
-        
         assert 'Authorization' in self.getHeader
         response = requests.get(url, headers=self.getHeader)
-
-        assert response.status_code == 200
+        status_codes = self.response_status_codes
+        status_codes[401] = "Returned if your JWT token is missing or expired"
+        self.__handleException(status_codes, response.status_code)
         self.token = response.json()['token']
         self.getHeader['Authorization'] = 'Bearer ' + self.token
+
+    def episodes(self, id):
+        url = self.url + '/episodes/{}'.format(id)
+        assert 'Authorization' in self.getHeader
+        response = requests.get(url, headers=self.getHeader)
+        status_codes = self.response_status_codes
+        status_codes[401] = "Returned if your JWT token is missing or expired"
+        status_codes[404] = "Returned if the given episode ID does not exist"
+        self.__handleException(status_codes, response.status_code)
+        assert response.headers.get('content-type') == 'application/json; charset=utf-8'
+        return response.json()
