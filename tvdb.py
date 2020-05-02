4eac34a9bc0681a48fb319aeaca882d674b94eec
nelson-caberto <nelson-caberto@users.noreply.github.com>
2020-05-01 23:20:39 -0500

    inital commit

---
+++ tvdb.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+class tvdb:
+
+    url = 'https://api.thetvdb.com'
+
+    postHeader = {'Content-Type': 'application/json'}
+    getHeader = {'Accept': 'application/json'}
+
+    token = ''
+
+    response_status_codes = {
+        200:"success"
+    }
+
+    def __init__(self, userkey, apikey, username):
+        self.userkey = userkey
+        self.apikey = apikey
+        self.username = username
+        self.login()
+
+    def login(self):
+        url = self.url + '/login'
+        data = {
+            'userkey':self.userkey,
+            'apikey':self.apikey,
+            'username':self.username
+        }
+        response = requests.post(url, data=json.dumps(data), headers=self.postHeader)
+        status_code = response.status_code
+        status_codes = self.response_status_codes
+        status_codes[401] = "Invalid credentials and/or API token"
+
+        if (status_code != 200):
+            raise Exception(status_codes[status_code])
+
+        self.token = response.json()['token']
+        self.getHeader['Authorization'] = 'Bearer ' + self.token
+
+    def refresh_token(self):
+        url = self.url + '/refresh_token'
+        #assert getHeader already has Authorization Bearer token
+        response = requests.get(url, headers=self.getHeader)
+        #assert response status is 200
+        self.token = response.json()['token']
+        self.getHeader['Authorization'] = 'Bearer ' + self.token
